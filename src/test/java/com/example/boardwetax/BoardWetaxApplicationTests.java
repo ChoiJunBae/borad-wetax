f4509932957454933d30affb5ab36cf6125d4e77
ChoiJunBae <cjb9610@naver.com>
2022-01-12 14:50:17 +0900

    first commit

---
+++ src/test/java/com/example/boardwetax/BoardWetaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardwetax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardWetaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
